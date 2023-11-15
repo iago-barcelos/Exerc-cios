5b600f3c1eac362af3f19f3da6327fc248a1068f
iago-barcelos <iago.cmpb@gmail.com>
2023-11-15 11:36:11 -0300

    primeiras configurações do exercício do dia

---
+++ back-end/secao-4/dia-4/teams/src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+let nextId = 3;
+const teams = [
+  { id: 1, nome: 'São Paulo Futebol Clube', sigla: 'SPF' },
+  { id: 2, nome: 'Sociedade Esportiva Palmeiras', sigla: 'PAL' },
+];
+
+app.get('/teams/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find(t => t.id === id);
+  if (team) {
+    res.json(team);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+module.exports = app;
---
+++ back-end/secao-4/dia-4/teams/src/server.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+app.listen(3001, () => {
+  console.log(`server runnig on port 3001`)
+})
